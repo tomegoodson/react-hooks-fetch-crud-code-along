aba58b6afa099a538c2a076cc8807d41b5632673
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-04-03 11:16:36 -0500

    crudlab.tg

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,34 +1,49 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        category,
+      }),
+    })
+      .then((r) => r.json())
+      .then((newItem) => {
+        onAddItem(newItem); 
+        setName(""); 
+        setCategory("Produce"); 
+      })
+      .catch((error) => console.error("Error adding item:", error));
+  }
+
   return (
-    <form className="NewItem">
+    <form onSubmit={handleSubmit} className="NewItem">
       <label>
         Name:
         <input
           type="text"
-          name="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </label>
-
       <label>
         Category:
-        <select
-          name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
-
       <button type="submit">Add to List</button>
     </form>
   );
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,26 +7,60 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
 
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]); 
+  }
+
+  function handleDeleteItem(deletedItem) {
+    fetch(`http://localhost:4000/items/${deletedItem.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+        setItems(updatedItems);
+      })
+      .catch((error) => console.error("Error deleting item:", error));
+  }
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
 
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) =>
+      item.id === updatedItem.id ? updatedItem : item
+    );
+    setItems(updatedItems);
+  }
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} /> {}
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item
+            key={item.id}
+            item={item}
+            onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
